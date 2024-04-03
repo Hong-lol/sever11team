0e725b8e0d2459b2c0d461e45427666824ebe32f
hwangjunseong <junseong5832@gmail.com>
2024-04-03 17:14:50 +0900

    .

--- MY_ME/main.py
+++ MY_ME/main.py
@@ -5,9 +5,4 @@ app = create_app()
 if __name__ == '__main__':
     app.run(debug=True)
 
-<<<<<<< HEAD
-# huhyunseo
-# hwangjunseong
-=======
-#hyeonwoo
->>>>>>> b2bb52db7566bd4157f49d5bea30a415c79dd64d
+# 아무거나
